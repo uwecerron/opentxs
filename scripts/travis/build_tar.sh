6ebe4e6688b07ad2d3a1c3646ab6a1971a87d033
Tommy Back <tommy.back@iki.fi>
2014-06-18 16:13:49 +0200

    Improved Travis packaging.

--- scripts/travis/build_tar.sh
+++ scripts/travis/build_tar.sh
@@ -1,26 +1,35 @@
 #!/usr/bin/env bash
-set -x
+set -e
+
+os="linux"
+if [[ -z "$1" ]] ; then
+    os=$1
+fi
+
+build="0"
+if [[ -z "$2" ]] ; then
+    build="$2"
+fi
 
 mkdir package
 
-cp -rf $TRAVIS_BUILD_DIR/include $TRAVIS_BUILD_DIR/package/
-cp -rf $TRAVIS_BUILD_DIR/src/otlib/.libs/libot.so $TRAVIS_BUILD_DIR/package/
-cp -rf $TRAVIS_BUILD_DIR/src/otlib/.libs/libot.so.0 $TRAVIS_BUILD_DIR/package/
-cp -rf $TRAVIS_BUILD_DIR/src/otlib/.libs/libot.so.0.0.0 $TRAVIS_BUILD_DIR/package/
-cp -rf $TRAVIS_BUILD_DIR/src/otextensions/.libs/libotextensions.so $TRAVIS_BUILD_DIR/package/
-cp -rf $TRAVIS_BUILD_DIR/src/otextensions/.libs/libotextensions.so.0 $TRAVIS_BUILD_DIR/package/
-cp -rf $TRAVIS_BUILD_DIR/src/otextensions/.libs/libotextensions.so.0.0.0 $TRAVIS_BUILD_DIR/package/
-cp -rf $TRAVIS_BUILD_DIR/src/otapi/.libs/libotapi.so $TRAVIS_BUILD_DIR/package/
-cp -rf $TRAVIS_BUILD_DIR/src/otapi/.libs/libotapi.so.0 $TRAVIS_BUILD_DIR/package/
-cp -rf $TRAVIS_BUILD_DIR/src/otapi/.libs/libotapi.so.0.0.0 $TRAVIS_BUILD_DIR/package/
-cp -rf $TRAVIS_BUILD_DIR/src/.libs/opentxs $TRAVIS_BUILD_DIR/package/
+cp -rf include package/
+cp -rf src/otlib/.libs/libot.so package/
+cp -rf src/otlib/.libs/libot.so.0 package/
+cp -rf src/otlib/.libs/libot.so.0.0.0 package/
+cp -rf src/otextensions/.libs/libotextensions.so package/
+cp -rf src/otextensions/.libs/libotextensions.so.0 package/
+cp -rf src/otextensions/.libs/libotextensions.so.0.0.0 package/
+cp -rf src/otapi/.libs/libotapi.so package/
+cp -rf src/otapi/.libs/libotapi.so.0 package/
+cp -rf src/otapi/.libs/libotapi.so.0.0.0 package/
+cp -rf src/.libs/opentxs package/
 
 version=`cat VERSION`
 compiler=${CXX}-`${CXX} -dumpversion`
-os=$TRAVIS_OS_NAME
-package="opentxs-${version}-${TRAVIS_BUILD_NUMBER}-${compiler}-${os}.tar.gz"
+package="opentxs-${version}-${build}-${compiler}-${os}.tar.gz"
 
 tar -vpczf $package package/
 
 mkdir s3
-cp ${package} $TRAVIS_BUILD_DIR/s3/
+cp ${package} s3/

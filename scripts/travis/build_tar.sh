fb26256f50539de8217f9307082deb0cad30f38c
Tommy Back <tommy.back@iki.fi>
2014-06-18 08:45:22 +0200

    Improved naming of package that Travis builds and uploads to S3.

--- scripts/travis/build_tar.sh
+++ scripts/travis/build_tar.sh
@@ -15,7 +15,11 @@ cp -rf $TRAVIS_BUILD_DIR/src/otapi/.libs/libotapi.so.0 $TRAVIS_BUILD_DIR/package
 cp -rf $TRAVIS_BUILD_DIR/src/otapi/.libs/libotapi.so.0.0.0 $TRAVIS_BUILD_DIR/package/
 cp -rf $TRAVIS_BUILD_DIR/src/.libs/opentxs $TRAVIS_BUILD_DIR/package/
 
-tar -vpczf "package.tar.gz" $TRAVIS_BUILD_DIR/package/
+version=`cat VERSION`
+compiler=${CXX}-`${CXX} -dumpversion`
+package="opentxs-${version}-${TRAVIS_BUILD_NUMBER}-${compiler}.tar.gz"
+
+tar -vpczf $package $TRAVIS_BUILD_DIR/package/
 
 mkdir s3
-cp package.tar.gz $TRAVIS_BUILD_DIR/s3/package.tar.gz
+cp ${package} $TRAVIS_BUILD_DIR/s3/
